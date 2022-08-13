07ac8ff3db9789a7b09e63416bd86be52f0de897
Republicsandy <sandy180388@gmail.com>
2022-08-13 16:00:17 +0530

    [sandeep] fibonacci file added in  UC_1

---
+++ Logical_Programs/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+     class FibonacciSeries
+    {
+        public FibonacciSeries()
+        {
+            Console.WriteLine("Enter number for finding the series");
+
+            int number =Convert.ToInt32(Console.ReadLine());
+            int valueInFirstNumber = 0, valueInSecondNumber = 1, valueInThirdNumber = 2;
+            
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(valueInFirstNumber + " ");
+
+                valueInThirdNumber = valueInFirstNumber + valueInSecondNumber;
+                valueInFirstNumber = valueInSecondNumber;
+                valueInSecondNumber = valueInThirdNumber;
+            }
+        }
+    }
+}
---
+++ Logical_Programs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Logical Problem Series");
+            Console.WriteLine("Please Enter the Number Which Program you want to Perform");
+            Console.WriteLine("Press 1 for Fibonacci");
+
+            int valueGet = Convert.ToInt32(Console.ReadLine());
+            
+            switch(valueGet)
+            {
+                case 1:
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    break;
+            }
+        }
+    }
+}
